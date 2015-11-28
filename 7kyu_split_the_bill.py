f8722ec4f73ccfe718a0df8d45d4434bc5e84d35
kucuny <kucuny@gmail.com>
2015-11-28 11:12:12 +0900

    7kyu split the bill

---
+++ 7kyu_split_the_bill.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+def split_the_bill(x):
+    avg = sum(x.values()) / len(x)
+    return {key: value - avg for key, value in x.items()}
+
+
+class TestSplitTheBill(unittest.TestCase):
+    def test_split_the_bill_is_valid(self):
+        self.assertDictEqual(split_the_bill({'A': 20, 'B': 15, 'C': 10}), {'A': 5, 'B': 0, 'C': -5})
+        self.assertDictEqual(split_the_bill({'A': 40, 'B': 25, 'X': 10}), {'A': 15, 'B': 0, 'X': -15})
